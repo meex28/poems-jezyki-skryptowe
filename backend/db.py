9ce27e41d177952eac82319e065082630a3f3af0
meex28 <71528905+meex28@users.noreply.github.com>
2022-06-06 18:24:00 +0200

    change db conn

--- backend/db.py
+++ backend/db.py
@@ -129,6 +129,7 @@ class Opinion(Base):
 # engine = create_engine('mysql+pymysql://root:pass@127.0.0.1:3306/daily-poem-db')
 
 # connection to heroku DB
-engine = create_engine('mysql+pymysql://bd7fa6bea46609:28f6dd83@eu-cdbr-west-02.cleardb.net/heroku_5cdeb3c4117633f')
+engine = create_engine('mysql+pymysql://bd7fa6bea46609:28f6dd83@eu-cdbr-west-02.cleardb.net/heroku_5cdeb3c4117633f',
+                       pool_size=100)
 
 Base.metadata.create_all(bind=engine)
